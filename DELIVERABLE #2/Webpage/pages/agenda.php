d7274ec2cf81e5dd892ae69bfd068c5a4e4b2340
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2017-10-31 12:35:23 +0000

    Dynamic table created and put on agenda

---
+++ DELIVERABLE #2/Webpage/pages/agenda.php
@@ -0,0 +1,17 @@
+<?php
+$table_headers = ["Rendering engine","Browser","Platform(s)","Engine version","CSS grade"];
+$entry = [
+		"Rendering engine"=> "Trident",
+		"Browser"=>"Internet Explorer 4.0",
+		"Platform(s)"=>"Win 95+",
+		"Engine version"=>"4",
+		"CSS grade"=>"X"
+	];
+$entries = [];
+for ($i=0; $i < 1000; $i++)
+	array_push($entries, $entry);
+
+	include('../templates/header.php');
+	include('../templates/table.php');
+	include('../templates/footer.php');
+?>
