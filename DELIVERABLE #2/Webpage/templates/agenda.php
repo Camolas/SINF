8a068b137e3436fdfc1c4f0eabe5931bdc44d569
Camolas <manuelcurral@gmail.com>
2017-11-02 19:52:37 +0000

    Alterações
    Organização do código , PAGES e TEMPLATES

---
+++ DELIVERABLE #2/Webpage/templates/agenda.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Xpto
+ * Date: 02/11/2017
+ * Time: 19:25
+ */?>
+
+<div id="wrapper">
+    <!-- Navigation -->
+
+
+    <!-- database -->
+
+
+    <!-- CONTEÚDO -->
+
+    <div id="page-wrapper">
+
+        <!-- /.row -->
+        <div class="row">
+            <div class="col-lg-8">
+                <!-- /.panel-heading -->
+
+                <div class="panel-body">
+                    <div class="list-group">
+
+                        <iframe src="https://calendar.google.com/calendar/embed?src=dmcpp8gtvgiksb753qfi4u9qfc%40group.calendar.google.com&ctz=Europe/Lisbon"
+                                style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
