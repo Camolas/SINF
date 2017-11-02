3cb4eddc1b0e941ee3296d244f2bcfffebc78b4b
Camolas <manuelcurral@gmail.com>
2017-11-02 17:14:01 +0000

    Calendar prototype

--- DELIVERABLE #2/Webpage/pages/agenda.php
+++ DELIVERABLE #2/Webpage/pages/agenda.php
@@ -15,3 +15,43 @@ for ($i=0; $i < 1000; $i++)
 	include('../templates/table.php');
 	include('../templates/footer.php');
 ?>
+
+<div id="wrapper">
+    <!-- Navigation -->
+
+
+    <!-- database -->
+
+
+    <!-- CONTEÚDO -->
+
+    <div id="page-wrapper">
+
+<!-- /.row -->
+<div class="row">
+    <div class="col-lg-8">
+<!-- /.panel-heading -->
+
+<div class="panel-body">
+    <div class="list-group">
+
+        <iframe src="https://calendar.google.com/calendar/embed?src=dmcpp8gtvgiksb753qfi4u9qfc%40group.calendar.google.com&ctz=Europe/Lisbon" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+        <a href="#" class="list-group-item">
+
+            <i class="fa fa-comment fa-fw"></i> Novo Comentário
+            <span class="pull-right text-muted small"><em>4 minutos atrás</em>
+                                    </span>
+        </a>
+
+        <a href="account.php?msg=1" class="list-group-item">
+            <i class="fa fa-envelope fa-fw"></i> Mensagem enviada
+            <span class="pull-right text-muted small"><em>10 minutos atrás</em>
+                                    </span>
+        </a>
+        <a href="#" class="list-group-item">
+            <i class="fa fa-tasks fa-fw"></i> Nova Tarefa
+            <span class="pull-right text-muted small"><em>09:49</em>
+                                    </span>
+        </a>
+
+    </div>
