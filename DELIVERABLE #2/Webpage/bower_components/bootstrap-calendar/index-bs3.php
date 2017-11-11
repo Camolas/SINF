b64c1ce31ecfec4d7599845318c40c55cc331790
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-11-11 22:01:36 +0000

    stage dependencies

---
+++ DELIVERABLE #2/Webpage/bower_components/bootstrap-calendar/index-bs3.php
@@ -0,0 +1,58 @@
+	<div class="page-header">
+
+		<div class="pull-right form-inline">
+			<div class="btn-group">
+				<button class="btn btn-primary" data-calendar-nav="prev">Prev</button>
+				<button class="btn btn-default" data-calendar-nav="today">Today</button>
+				<button class="btn btn-primary" data-calendar-nav="next">Next</button>
+			</div>
+			<div class="btn-group">
+				<button class="btn btn-warning" data-calendar-view="year">Year</button>
+				<button class="btn btn-warning active" data-calendar-view="month">Month</button>
+				<button class="btn btn-warning" data-calendar-view="week">Week</button>
+				<button class="btn btn-warning" data-calendar-view="day">Day</button>
+			</div>
+		</div>
+
+		<h3></h3>
+		<small>To see example with events navigate to march 2013</small>
+	</div>
+
+	<div class="row">
+			<div id="calendar"></div>
+	</div>
+
+	<div class="clearfix"></div>
+	<br><br>
+
+	<div class="modal fade" id="events-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h3 class="modal-title">Event</h3>
+				</div>
+				<div class="modal-body" style="height: 400px">
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script type="text/javascript" src="http://localhost/git/DELIVERABLE%20%232/Webpage/bower_components/underscore/underscore-min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<script type="text/javascript" src="http://localhost/git/DELIVERABLE%20%232/Webpage/bower_components/bootstrap-calendar/js/calendar.js"></script>
+	<script type="text/javascript" src="http://localhost/git/DELIVERABLE%20%232/Webpage/bower_components/bootstrap-calendar/js/app.js"></script>
+
+	<script type="text/javascript">
+		var disqus_shortname = 'bootstrapcalendar'; // required: replace example with your forum shortname
+		(function() {
+			var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+			dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+		})();
+	</script>
