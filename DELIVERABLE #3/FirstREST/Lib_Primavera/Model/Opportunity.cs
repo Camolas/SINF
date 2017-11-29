9877cab4106edc72f956c4a0cfbdc837128f90cc
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2017-11-29 15:26:32 +0000

    Missing change related with 'Using state on opportunities'

--- DELIVERABLE #3/FirstREST/Lib_Primavera/Model/Opportunity.cs
+++ DELIVERABLE #3/FirstREST/Lib_Primavera/Model/Opportunity.cs
@@ -44,7 +44,7 @@ namespace FirstREST.Lib_Primavera.Model
             set;
         }
 
-        public string state
+        public string opportunity_state
         {
             get;
             set;
